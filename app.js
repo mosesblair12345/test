81f5f445e86b6a8d062e54b8eaa06c0cc128e0a6
moses_blair <mosesblair00@gmail.com>
2023-08-22 14:44:50 +0300

    v2

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const port = 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.post("/callback",(req,res)=>{
     console.log(req.body);
 })
